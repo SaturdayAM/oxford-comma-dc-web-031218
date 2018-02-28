8c608711c614eb8d45539aaf17b3afc66459971f
Jerry Lee <jwl62@georgetown.edu>
2018-02-28 20:51:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,12 @@
 def oxford_comma(array)
-  last = array.pop
-  to_return = array.join(", ")
-  to_return += ", and #{last}"
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    last = array.pop
+    to_return = array.join(", ")
+    to_return += ", and #{last}"
+    return to_return
+  end
 end
